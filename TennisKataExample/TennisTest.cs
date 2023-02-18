5ab05805c67c8fe06abfe5e40f15f970163ca49a
Ligi <ligiso@gmail.com>
2023-02-18 22:50:18 +0800

    01.測試綠燈後重構，邊改邊測 zxf tennis 抽成欄位 _tennis zxm 重構成方法 zxp 抽成方法參數 expected ztl 執行測試
    nmap zxf <Action>(IntroduceField)
    vmap zxm <Action>(ExtractMethod)/
    nmap zxp <Action>(IntroduceParameter)
    nmap ztl <Action>(RiderUnitTestRunSolutionAction)

--- TennisKataExample/TennisTest.cs
+++ TennisKataExample/TennisTest.cs
@@ -2,15 +2,22 @@ namespace TennisKataExample;
 
 public class TennisTest
 {
+    private Tennis _tennis;
+
     [SetUp]
     public void Setup()
     {
+        _tennis = new Tennis();
     }
 
     [Test]
     public void Love_All()
     {
-        var tennis = new Tennis();
-        Assert.AreEqual("Love All", tennis.Score());
+        ScoreShouldBe("Love All");
+    }
+
+    private void ScoreShouldBe(string expected)
+    {
+        Assert.AreEqual(expected, _tennis.Score());
     }
 }
