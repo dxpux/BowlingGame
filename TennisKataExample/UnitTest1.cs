13c5920af9b118bd24d98b0d16a31db8cd12a6b3
Ligi <ligiso@gmail.com>
2023-02-18 22:09:07 +0800

    00.Exmaple Proj init

---
+++ TennisKataExample/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace TennisKataExample;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
