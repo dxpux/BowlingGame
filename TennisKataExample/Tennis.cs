e62ca0e016ec0f8bb5c0acbe64abc6b3717ffadc
Ligi <ligiso@gmail.com>
2023-02-19 18:03:16 +0800

    02.通過 Fifteen_Love 測試 過程中避免多餘的排版，執行測試(ztl)的空檔，格式化(zed)文件即可 nmap zed <Action>(ReformatCode)

--- TennisKataExample/Tennis.cs
+++ TennisKataExample/Tennis.cs
@@ -2,13 +2,20 @@ namespace TennisKataExample;
 
 public class Tennis
 {
+    private int _firstPlayerScoreTimes;
+
     public string Score()
     {
+        if (_firstPlayerScoreTimes == 1)
+        {
+            return "Fifteen Love";
+        }
+
         return "Love All";
     }
 
     public void FirstPlayerScore()
     {
-        throw new NotImplementedException();
+        _firstPlayerScoreTimes++;
     }
 }
