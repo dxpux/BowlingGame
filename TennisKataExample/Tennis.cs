eee733ae044924157e91bf8c3177fa560b97add5
Ligi <ligiso@gmail.com>
2023-02-22 23:49:23 +0800

    04.綠燈後重構 Product Code

--- TennisKataExample/Tennis.cs
+++ TennisKataExample/Tennis.cs
@@ -4,17 +4,18 @@ public class Tennis
 {
     private int _firstPlayerScoreTimes;
 
+    private readonly Dictionary<int, string> _scoreLookup = new Dictionary<int, string>()
+    {
+        { 1, "Fifteen" },
+        { 2, "Thirty" },
+        { 3, "Forty" },
+    };
+
     public string Score()
     {
-        var scoreLookup = new Dictionary<int, string>()
-        {
-            { 1, "Fifteen" },
-            { 2, "Thirty" },
-            { 3, "Forty" },
-        };
         if (_firstPlayerScoreTimes > 0)
         {
-            return $"{scoreLookup[_firstPlayerScoreTimes]} Love";
+            return $"{_scoreLookup[_firstPlayerScoreTimes]} Love";
         }
 
         return "Love All";
